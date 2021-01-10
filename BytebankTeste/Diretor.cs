74d1b8efe74fbf0388ae02c0a8db8d24b4e32a35
Davi Vasconcelos <vdrdavi@gmail.com>
2021-01-10 13:16:10 -0500

    Primeiro commit do projeto

---
+++ BytebankTeste/Diretor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank_.Funcionarios
+{
+    class Diretor : Funcionario
+    {
+        public override double GetBonificacao()
+        {
+            return 0.5 * Salario;
+        }
+
+        public Diretor(string cpf) : base(5000, cpf)
+        {
+        }
+
+        public override void AumentarSalario()
+        {
+            Salario *= 1.15;
+        }
+    }
+}
---
+++ BytebankTeste/Funcionarios/Funcionario.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank_.Funcionarios
+{
+    public abstract class  Funcionario
+    {
+        public string Nome { get; set; }
+        public string CPF { get; private set; }
+        public double Salario { get; protected set; }
+        public static int TotalDeFuncionarios { get; private set; }
+
+        public Funcionario(double salario, string cpf)
+        {
+            Salario = salario;
+            cpf = CPF;
+
+            TotalDeFuncionarios++;
+            // Construtores da classe base sempre são invocados primeiro
+            Console.WriteLine("Construtor de FUNCIONARIO");
+        }
+
+        public abstract void AumentarSalario();
+
+        public abstract double GetBonificacao();
+    }
+}
